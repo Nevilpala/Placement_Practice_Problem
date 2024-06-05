336574feee794b5ded1ba5e88fc209a2ed394d5d
NEVIL PALA <93420116+Nevilpala@users.noreply.github.com>
2024-06-05 11:18:08 +0530

    Add files via upload

--- Test2.java
+++ Test2.java
@@ -1,4 +1,67 @@
+import java.util.Scanner;
 
+class Test2{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("=> Program - 2 <=");
+        System.out.println();
+
+        
+        // System.out.print("Size n : ");
+        // int n = sc.nextInt();
+
+        // int[] arr = new int[n];
+
+        // System.out.println();
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print("array[" + (i) + "] : ");
+        //     arr[i] = sc.nextInt();
+        // }
+        
+
+        // System.out.println("\nAns : " + jumpLength(arr, n));
+        System.out.println("1 for Minstack"); 
+        System.out.println("2 for push"); 
+        System.out.println("3 for pop"); 
+        System.out.println("4 for top"); 
+        System.out.println("5 for getMin"); 
+
+        System.out.print("\nEnter Operaton with comma seprated : "); 
+        String[] word = sc.nextLine().split(",");
+
+        System.out.print("Enter Value with comma seprated : "); 
+        String[] val = sc.nextLine().split(",");
+        
+    
+        if(word.length == val.length){
+            MinStack minStack = new MinStack();
+            String[] st = new String[word.length];
+            for(int i = 0;i<word.length;i++){
+                String ans = "";
+                switch (word[i]) {
+                    case "1":
+                        minStack = new MinStack();
+                        break;
+                    case "2":
+                        minStack.push();;
+                        break;
+                    case "3":
+                        minStack = new MinStack();
+                        break;
+                    case "4":
+                        minStack = new MinStack();
+                        break;
+                    case "5":
+                        minStack = new MinStack();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
 class MinStack{
     int arr[] = new int[100],top=-1;
     public void push(int val){
@@ -32,10 +95,5 @@ class MinStack{
 
         }
         return min;
-    }
-}
-class Test2{
-    public static void main(String[] args){
-        
     }
 }
